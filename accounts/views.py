327fb3a1cccdf763d701909ccf6871c4884a81d7
anarchyroot <griewing15>
2017-07-19 10:31:32 +0100

    background image, containers and layout for login, register pages.

--- accounts/views.py
+++ accounts/views.py
@@ -9,62 +9,62 @@ from accounts.forms import UserRegistrationForm, UserLoginForm
 
 @login_required(login_url='/accounts/login')
 def profile(request):
-    return render(request, 'profile.html')
+	return render(request, 'profile.html')
 
 
 def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request.POST)
-        if form.is_valid():
-            user = auth.authenticate(username=request.POST.get('username_or_email'),
-                                     password=request.POST.get('password'))
+	if request.method == 'POST':
+		form = UserLoginForm(request.POST)
+		if form.is_valid():
+			user = auth.authenticate(username=request.POST.get('username_or_email'),
+			                         password=request.POST.get('password'))
 
-            if user is not None:
-                auth.login(request, user)
-                messages.error(request, "You have successfully logged in")
+			if user is not None:
+				auth.login(request, user)
+				messages.error(request, "You have successfully logged in")
 
-                if request.GET and 'next' in request.GET:
-                    next = request.GET['next']
-                    return HttpResponseRedirect(next)
-                else:
-                    return redirect(reverse('profile'))
-            else:
-                form.add_error(None, "Your username or password was not recognised")
-    else:
-        form = UserLoginForm()
+				if request.GET and request.GET['next'] != '':
+					next = request.GET['next']
+					return HttpResponseRedirect(next)
+				else:
+					return redirect(reverse('index'))
+			else:
+				form.add_error(None, "Your username or password was not recognised")
+	else:
+		form = UserLoginForm()
 
-    args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
-    args.update(csrf(request))
-    return render(request, 'login.html', args)
+	args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
+	args.update(csrf(request))
+	return render(request, 'login.html', args)
 
 
 def logout(request):
-    auth.logout(request)
-    messages.success(request, 'You have successfully logged out')
-    return redirect(reverse('index'))
+	auth.logout(request)
+	messages.success(request, 'You have successfully logged out')
+	return redirect(reverse('index'))
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
+	if request.method == 'POST':
+		form = UserRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
 
-            user = auth.authenticate(username=request.POST.get('username'),
-                                     password=request.POST.get('password1'))
+			user = auth.authenticate(username=request.POST.get('username'),
+			                         password=request.POST.get('password1'))
 
-            if user:
-                auth.login(request, user)
-                messages.success(request, "You have successfully registered")
-                return redirect(reverse('profile'))
+			if user:
+				auth.login(request, user)
+				messages.success(request, "You have successfully registered")
+				return redirect(reverse('profile'))
 
-            else:
-                messages.error(request, "unable to log you in at this time!")
+			else:
+				messages.error(request, "unable to log you in at this time!")
 
-    else:
-        form = UserRegistrationForm()
+	else:
+		form = UserRegistrationForm()
 
-    args = {'form': form}
-    args.update(csrf(request))
+	args = {'form': form}
+	args.update(csrf(request))
 
-    return render(request, 'register.html', args)
+	return render(request, 'register.html', args)
