9a5e87bd0357acd16c2a04af5428b11788759662
anarchyroot <griewing15>
2017-07-29 20:38:55 +0100

    downloaded from server

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from products.models import Product
 
 # Create your views here.
 def get_index(request):
-    return render(request, "index.html")
+    products = Product.objects.all()
+    return render(request, "index.html", {'products': products})
